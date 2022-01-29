c2b954451b6435d658b202aab0555861936c1803
prabhkaur301 <you@example.com>
2022-01-29 12:08:28 +0530

    adding image in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,8 @@
+import image from "../images/troll-face.svg"
 export default function Header(){
     return(
         <header className="header">
-            <img src="/images/Troll Face.svg" alt=""></img>
+            <img src={image} alt=""></img>
             <h1>Meme Generator</h1>
             <p className="right">React Course Project-3</p>
             
