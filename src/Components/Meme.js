0568fb050f2735223ce0071687b3f0ac44f6a348
prabhkaur301 <pk746467@gmail.com>
2023-05-21 23:48:30 +0530

    [update] styling settings

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -1,163 +1,171 @@
-import React from "react"
-import domtoimage from 'dom-to-image'
-import icon from "../images/download-solid.svg"
+import React, { useEffect, useState } from "react";
+import domtoimage from "dom-to-image";
+import icon from "../images/download-solid.svg";
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    top: {
+      text: "",
+      styles: {},
+    },
+    bottom: {
+      text: "",
+      styles: {},
+    },
+    randomImg: "https://i.imgflip.com/30b1gx.jpg",
+  });
+  const [activeStyle, setActiveStyle] = useState("");
+  const [memeImgData, setMemeImgData] = useState([]);
+  const [slider, setSlider] = useState({
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    fontSize: 16,
+  });
+  const sliderData = [
+    { label: "Top: ", value: "top" },
+    { label: "Left: ", value: "left" },
+    { label: "Right: ", value: "right" },
+    { label: "Bottom: ", value: "bottom" },
+    { label: "Font Size: ", value: "fontSize" },
+  ];
 
-    // state for managing meme
-    const [meme, setMeme] = React.useState({
-        topText: "",
-        bottomText: "",
-        randomImg: "https://i.imgflip.com/30b1gx.jpg"
-    })
-    // state for managing img data
-    const [memeImgData, setMemeImgData] = React.useState([]);
+  function handleSliderChange(e) {
+    const { name, value } = e.target;
+    setSlider((prevData) => ({ ...prevData, [name]: value }));
+  }
 
-    const [slider, setSlider] = React.useState({
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        fontSize: 16
-    })
-    const labels = ["Top: ", "Left: ", "Right: ", "Bottom: ", "Font-Size: "]
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevData) => ({
+      ...prevData,
+      [name]: {
+        ...prevData[name],
+        text: value,
+      },
+    }));
+  }
 
-    // useEffect for getting data from Api
+  useEffect(() => {
+    const textStyles = {
+      top: `${slider.top}%`,
+      right: `${slider.right}%`,
+      fontSize: `${slider.fontSize}px`,
+      left: `${slider.left}%`,
+      bottom: `${slider.bottom}%`,
+    };
 
-    React.useEffect(() => {
-        fetch(" https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(apiData => setMemeImgData(apiData.data.memes))
-    }, [])
+    setMeme((prevData) => ({
+      ...prevData,
+      [activeStyle]: {
+        ...prevData[activeStyle],
+        styles: textStyles,
+      },
+    }));
+  }, [slider, activeStyle]);
 
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((apiData) => setMemeImgData(apiData.data.memes));
+  }, []);
 
+  function getImage() {
+    const randomNo = Math.floor(Math.random() * memeImgData.length);
+    const imgUrl = memeImgData[randomNo].url;
+    setMeme((prev) => ({ ...prev, randomImg: imgUrl }));
+  }
 
-    function getImage() {
-        const dataArr = memeImgData
-        const randomNo = Math.floor(Math.random() * dataArr.length);
-        const imgUrl = dataArr[randomNo].url;
-        setMeme((prev) => {
-            return ({ ...prev, randomImg: imgUrl })
-        })
-    }
-    function handleSliderChange(e) {
-        const { name, value } = e.target
-        setSlider((prevData) => {
-            return ({ ...prevData, [name]: value })
-        })
-    }
-    function handleChange(event) {
-        const { name, value } = event.target;
-        setMeme((prevData) => {
-            return ({
-                ...prevData, [name]: value
-            })
-        })
-    }
+  function downloadImg() {
+    domtoimage
+      .toJpeg(document.getElementById("image"), { quality: 0.95 })
+      .then(function (dataUrl) {
+        var link = document.createElement("a");
+        link.download = "Meme.jpeg";
+        link.href = dataUrl;
+        link.click();
+      });
+  }
 
-    function downloadImg() {
-        domtoimage.toJpeg(document.getElementById('image'), { quality: 0.95 })
-            .then(function (dataUrl) {
-                var link = document.createElement('a');
-                link.download = 'Meme.jpeg';
-                link.href = dataUrl;
-                link.click();
-            });
-    }
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          className="input input-1"
+          placeholder="Enter top line"
+          name="top"
+          value={meme.top.text}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="input input-2"
+          placeholder="Enter bottom line"
+          name="bottom"
+          value={meme.bottom.text}
+          onChange={handleChange}
+        />
+        <button className="btn" onClick={getImage}>
+          Get a new meme image 🖼️
+        </button>
+      </div>
 
-    const topTextStyles = {
+      <div className="sys-container">
+        <div className="meme-img-container" id="image">
+          <h2 className="top-text meme-text" style={meme.top.styles}>
+            {meme.top.text}
+          </h2>
+          <h2 className="bottom-text meme-text" style={meme.bottom.styles}>
+            {meme.bottom.text}
+          </h2>
+          <img src={meme.randomImg} alt="" className="meme-img" />
+        </div>
 
-        top: `${slider.top}%`,
-        right: `${slider.right}%`,
-        fontSize: `${slider.fontSize}px`,
-        left: `${slider.left}%`
+        <div className="slider-container">
+          <div className="radio-container">
+            <p>Styles: </p>
+            <label>
+              Top
+              <input
+                type="radio"
+                name="style-type"
+                value="top"
+                checked={activeStyle === "top"}
+                onChange={() => setActiveStyle("top")}
+              />
+            </label>
+            <label>
+              Bottom
+              <input
+                type="radio"
+                name="style-type"
+                value="bottom"
+                checked={activeStyle === "bottom"}
+                onChange={() => setActiveStyle("bottom")}
+              />
+            </label>
+          </div>
+          {sliderData.map((item) => (
+            <React.Fragment key={item.value}>
+              <label htmlFor={item.value}>{item.label}</label>
+              <input
+                className={`${item.value} range-slider`}
+                type="range"
+                name={item.value}
+                value={slider[item.value]}
+                onChange={handleSliderChange}
+              />
+            </React.Fragment>
+          ))}
 
-
-
-    }
-    const bottomTextStyles =
-    {
-        bottom: `${slider.bottom}%`,
-        right: `${slider.right}%`,
-        fontSize: `${slider.fontSize}px`,
-        left: `${slider.left}%`
-
-    }
-
-    return (
-        <main>
-            <div className="form">
-                <input type="text"
-                    className="input input-1"
-                    placeholder="Enter top line"
-                    name="topText"
-                    value={meme.topText}
-                    onChange={handleChange}
-                />
-                <input type="text"
-                    className="input input-2"
-                    placeholder="Enter bottom line"
-                    name="bottomText"
-                    value={meme.bottomText}
-                    onChange={handleChange}
-                />
-                <button className="btn" onClick={getImage}>Get a new meme image 🖼️</button>
-
-            </div>
-
-
-
-            <div className="sys-container">
-
-                <div className="meme-img-container" id="image">
-                    <h2 className="top-text meme-text" style={topTextStyles}>{meme.topText}</h2>
-                    <h2 className="bottom-text meme-text" style={bottomTextStyles}>{meme.bottomText}</h2>
-                    <img src={meme.randomImg} alt="" className="meme-img" />
-                </div>
-                <div className="slider-container">
-                    <label htmlFor="top">{labels[0]}</label>
-                    <input className="top range-slider"
-                        type="range"
-                        name="top"
-                        value={slider.top}
-                        onChange={handleSliderChange}
-                    />
-                    <label htmlFor="left">{labels[1]}</label>
-                    <input className="left range-slider"
-                        type="range"
-                        name="left"
-                        value={slider.left}
-                        onChange={handleSliderChange}
-                    />
-                    <label htmlFor="bottom">{labels[3]}</label>
-                    <input className="bottom range-slider"
-                        type="range"
-                        name="bottom"
-                        value={slider.bottom}
-                        onChange={handleSliderChange}
-                    />
-                    <label htmlFor="right">{labels[2]}</label>
-                    <input className="right range-slider"
-                        type="range"
-                        name="right"
-                        value={slider.right}
-                        onChange={handleSliderChange}
-                    />
-                    <label htmlFor="font-size">{labels[4]}</label>
-                    <input className="font-size range-slider"
-                        type="range"
-                        name="fontSize"
-                        value={slider.fontSize}
-                        onChange={handleSliderChange}
-                    />
-
-
-                    <button id="download-btn" onClick={downloadImg} >
-                        Download Meme
-                        <img src={icon} id="icon" />
-                    </button>
-                </div>
-
-            </div>
-        </main>
-    )
+          <button id="download-btn" onClick={downloadImg}>
+            Download Meme
+            <img src={icon} id="icon" alt="icon" />
+          </button>
+        </div>
+      </div>
+    </main>
+  );
 }
